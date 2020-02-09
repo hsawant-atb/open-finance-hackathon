d3115eee9a842d8982715b4b50e93ed38bce6258
Sawant <hsawant@atb.com>
2020-02-08 19:32:10 -0700

    Added tasks list styling

--- bubblegum/screens/components/Task.js
+++ bubblegum/screens/components/Task.js
@@ -1,28 +1,29 @@
 import React from "react";
-import {
-    View,
-    Text,
-    StyleSheet
-} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { withOrientation } from "react-navigation";
 
-const Task = (props) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.taskTitle}>{props.taskTitle}</Text>
-            <Text style={styles.taskTitle}>{props.reward}</Text>
-            <Text style={styles.taskTitle}>{props.status}</Text>
-        </View>
-    )
-}
+const Task = props => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.taskTitle}>hi{props.taskTitle}</Text>
+      <Text style={styles.taskTitle}>hi{props.reward}</Text>
+      <Text style={styles.taskTitle}>hi{props.status}</Text>
+    </View>
+  );
+};
 export default Task;
 
 const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-        // alignItems: 'center',
-        // justifyContent: 'center'
-    },
-    taskTitle: {
-
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "grey",
+    shadowOpacity: 1,
+    elevation: 10,
+    borderRadius: 5,
+    paddingTop: 10,
+    flexDirection: "column"
+    // justifyContent: 'center'
+  },
+  taskTitle: {}
 });
--- bubblegum/screens/components/Taskgroup.js
+++ bubblegum/screens/components/Taskgroup.js
@@ -1,101 +1,96 @@
 import React, { Component } from "react";
-import {
-    View,
-    Text,
-    StyleSheet,
-    FlatList
-} from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
-import Task from './Task'
+import Task from "./Task";
 
-import * as firebase from 'firebase'
+import * as firebase from "firebase";
 
 const taskgroup = [
-    {
-        taskId: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-    },
-    {
-        taskId: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-    },
-    {
-        taskId: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-    },
+  {
+    taskId: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item"
+  },
+  {
+    taskId: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item"
+  },
+  {
+    taskId: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item"
+  }
 ];
 
 class TaskGroup extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props)
-    }
+  // componentDidMount() {
+  //     //Do your API calls here
 
+  //     const data = [] /* store the data in this object */
+  //     const parentId = "c0849637-cd77-4f9d-8409-ed959dbc1093"
+  //     const childId = "a9213f5d-bfc8-487f-8242-0789bbcc2c20"
 
-    // componentDidMount() {
-    //     //Do your API calls here
+  //     const db = firebase.database().ref(`/${parentId}/${childId}/taskList`)
 
-    //     const data = [] /* store the data in this object */
-    //     const parentId = "c0849637-cd77-4f9d-8409-ed959dbc1093"
-    //     const childId = "a9213f5d-bfc8-487f-8242-0789bbcc2c20"
+  //     db.once('value')
+  //         .then(function (snapshots) {
+  //             console.log(snapshots)
+  //             console.log(Object.keys(snapshots))
+  //             snapshots.forEach(function (data) {
+  //                 //console.log(1)
+  //                 const taskId = data.key
+  //                 const title = data.val().title
+  //                 const status = data.val().staus
+  //                 const reward = data.val().reward / 100
 
+  //                 const record = {
+  //                     taskId,
+  //                     title,
+  //                     status,
+  //                     reward
+  //                 }
 
-    //     const db = firebase.database().ref(`/${parentId}/${childId}/taskList`)
+  //                 data.push(record)
 
-    //     db.once('value')
-    //         .then(function (snapshots) {
-    //             console.log(snapshots)
-    //             console.log(Object.keys(snapshots))
-    //             snapshots.forEach(function (data) {
-    //                 //console.log(1)
-    //                 const taskId = data.key
-    //                 const title = data.val().title
-    //                 const status = data.val().staus
-    //                 const reward = data.val().reward / 100
+  //                 //console.log(record)
 
-    //                 const record = {
-    //                     taskId,
-    //                     title,
-    //                     status,
-    //                     reward
-    //                 }
+  //             })
+  //         })
 
-    //                 data.push(record)
+  //     //console.log(data)
+  // }
 
-    //                 //console.log(record)
-
-    //             })
-    //         })
-
-
-    //     //console.log(data)
-    // }
-
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Grades</Text>
-                <FlatList
-                    data={taskgroup}
-                    renderItem={({ item }) =>
-                        <Task
-                            taskTitle={item.title}
-                        />
-                    }
-
-                    keyExtractor={(item) => item.taskId}
-                />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Grades</Text>
+        <FlatList
+          data={taskgroup}
+          renderItem={({ item }) => <Task taskTitle={item.title} />}
+          keyExtractor={item => item.taskId}
+        />
+      </View>
+    );
+  }
 }
 export default TaskGroup;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    width: 380,
+    shadowOpacity: 1,
+    elevation: 10,
+    borderRadius: 20
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    padding: 20
+  }
 });
